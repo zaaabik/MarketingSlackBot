50ea83068205af0882865c831af11c54ddace260
zabik <zaaabik@gmail.com>
2017-09-04 16:12:33 +0300

    added userId for sendgrid and adding letters count commands

--- mbot/entities/userLettersCountJsonStruct.go
+++ mbot/entities/userLettersCountJsonStruct.go
@@ -4,10 +4,12 @@ type UserLettersCount struct {
 	HostId       string `json:"host_id"`
 	Provider     string `json:"provider"`
 	LettersCount string `json:"lettersCount"`
+	UserId       string `json:"user_id"`
 }
 
 type UserSendGrid struct {
 	HostId   string `json:"host_id"`
 	Provider string `json:"provider"`
 	Email    string `json:"email"`
+	UserId   string `json:"user_id"`
 }
